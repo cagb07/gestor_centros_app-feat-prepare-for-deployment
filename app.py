f4eaa9c2ba365a9ebfe8f59a676b99028893c81d
Cristian Granados B <154575206+cagb07@users.noreply.github.com>
2025-11-21 02:22:09 +0000

    5

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 ﻿import streamlit as st
 import pandas as pd
 import database
-            </style>
-        """, unsafe_allow_html=True)
-
-    # Navegación rápida entre vistas principales
-    st.sidebar.markdown("---")
 
 # --- APLICACIÓN PRINCIPAL (POST-LOGIN) ---
 def main_app():
